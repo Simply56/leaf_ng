e32f5461a2087f6cb9f2bd96697f9d3cdded02d3
Martin Srsen <xsrsen1@fi.muni.cz>
2025-07-06 12:31:34 +0200

    add create-plant component

---
+++ src/app/create-plant/create-plant.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreatePlant } from './create-plant';
+
+describe('CreatePlant', () => {
+  let component: CreatePlant;
+  let fixture: ComponentFixture<CreatePlant>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CreatePlant]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreatePlant);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/create-plant/create-plant.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-plant',
+  imports: [],
+  templateUrl: './create-plant.html',
+  styleUrl: './create-plant.css'
+})
+export class CreatePlant {
+
+}

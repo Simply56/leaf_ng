de6fa226a365bfb7909ca68649405b889ac9b590
Martin Sršeň <martin.srsen@sewio.net>
2025-07-09 15:54:11 +0200

    auto-formatting

--- src/app/plants-service.ts
+++ src/app/plants-service.ts
@@ -2,30 +2,29 @@ import { Injectable } from '@angular/core';
 import plantInfo from './models/plantInfo.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class PlantsService {
-  protected plantsList: plantInfo[] = [
-    {
-      id: 0,
-      name: "amelie",
-      imagePath: 'favicon.png',
-      lastWatered: new Date(2025, 6, 4)
-    },
-    {
-      id: 1,
-      name: "joline",
-      imagePath: 'favicon.png',
-      lastWatered: new Date()
+    protected plantsList: plantInfo[] = [
+        {
+            id: 0,
+            name: 'amelie',
+            imagePath: 'favicon.png',
+            lastWatered: new Date(2025, 6, 4),
+        },
+        {
+            id: 1,
+            name: 'joline',
+            imagePath: 'favicon.png',
+            lastWatered: new Date(),
+        },
+    ];
+
+    getAllPlants(): plantInfo[] {
+        return this.plantsList;
     }
-  ];
 
-  getAllPlants(): plantInfo[] {
-    return this.plantsList;
-  }
-
-  getPlantById(id: number): plantInfo | undefined {
-    return this.plantsList.find((p) => p.id === id);
-
-  }
+    getPlantById(id: number): plantInfo | undefined {
+        return this.plantsList.find((p) => p.id === id);
+    }
 }

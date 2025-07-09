71535dbce22c2f419e7430803418a1437c549333
Martin Srsen <xsrsen1@fi.muni.cz>
2025-07-10 00:12:51 +0200

    add network logic to create plant

--- src/app/create-plant/create-plant.ts
+++ src/app/create-plant/create-plant.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PlantsService } from '../plants-service';
 
 @Component({
-  selector: 'app-create-plant',
-  imports: [],
-  templateUrl: './create-plant.html',
-  styleUrl: './create-plant.css'
+    selector: 'app-create-plant',
+    imports: [],
+    templateUrl: './create-plant.html',
+    styleUrl: './create-plant.css'
 })
 export class CreatePlant {
+    service: PlantsService = inject(PlantsService);
 
 }
--- src/app/plants-service.ts
+++ src/app/plants-service.ts
@@ -17,11 +17,16 @@ export class PlantsService {
     url = 'http://localhost:8080';
 
     allPlants$ = (
-        this.http.get<plantInfo[]>(`${this.url}/plants`)
+        this.http.get<plantInfo[]>(`${this.url}/plants`).pipe(map(plants => plants.map(p => { p.imagePath = this.url + p.imagePath.slice(1); return p; })))
     );
 
 
     getPlantById(id: number) {
         return this.allPlants$.pipe(map((plants => plants.find((p => p.id == id)))));
+    }
+
+    createPlant(name: string) {
+        console.log(name);
+        this.http.post(`${this.url}/plants`, { name: name }).subscribe();
     }
 }

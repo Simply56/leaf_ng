556aa59968bd068e61f2f1cb1789c042562e3867
Martin Sršeň <martin.srsen@sewio.net>
2025-07-10 15:57:08 +0200

    syncing

--- src/app/plant-quick-info/plant-quick-info.ts
+++ src/app/plant-quick-info/plant-quick-info.ts
@@ -1,14 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ValueToColorPipe } from "../value-to-color-pipe";
+import { Component, Input, WritableSignal } from '@angular/core';
+import { ValueToColorPipe } from '../value-to-color-pipe';
 import plantInfo from '../models/plantInfo.model';
-import { ComputeWateredDaysAgoPipe } from "../compute-watered-days-ago-pipe";
+import { ComputeWateredDaysAgoPipe } from '../compute-watered-days-ago-pipe';
 import { RouterLink } from '@angular/router';
+import { PlantPicture } from '../plant-picture/plant-picture';
 
 @Component({
     selector: 'app-plant-quick-info',
-    imports: [ValueToColorPipe, ComputeWateredDaysAgoPipe, RouterLink],
+    imports: [
+        ValueToColorPipe,
+        ComputeWateredDaysAgoPipe,
+        RouterLink,
+        PlantPicture,
+    ],
     templateUrl: './plant-quick-info.html',
 })
 export class PlantQuickInfo {
-    @Input() plantInfo!: plantInfo;
+    @Input() plantInfo!: WritableSignal<plantInfo>;
+
+    submitted() {
+        console.log('submitted');
+    }
 }

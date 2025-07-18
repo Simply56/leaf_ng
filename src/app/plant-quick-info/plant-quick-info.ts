db6e98eb225c31b8ad70ea709f389a250f029c25
Martin Srsen <xsrsen1@fi.muni.cz>
2025-07-18 12:54:16 +0200

    fix line breaks in quick info

--- src/app/plant-quick-info/plant-quick-info.ts
+++ src/app/plant-quick-info/plant-quick-info.ts
@@ -14,6 +14,7 @@ import { PlantImage } from '../plant-image/plant-image';
         PlantImage
     ],
     templateUrl: './plant-quick-info.html',
+    styleUrl: './plant-quick-info.css'
 })
 export class PlantQuickInfo {
     @Input() plant!: plantInfo;

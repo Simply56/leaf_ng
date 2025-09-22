0edcb4ce1296f2a8c949bccfb706858a68ee5928
Martin Srsen <xsrsen1@fi.muni.cz>
2025-09-22 19:31:29 +0200

    remove form to avoid refreshig

--- src/app/components/create-plant/create-plant.ts
+++ src/app/components/create-plant/create-plant.ts
@@ -5,6 +5,7 @@ import { PlantsService } from '../../services/plants-service';
     selector: 'app-create-plant',
     imports: [],
     templateUrl: './create-plant.html',
+    styleUrl: './create-plant.css'
 })
 export class CreatePlant {
     service: PlantsService = inject(PlantsService);

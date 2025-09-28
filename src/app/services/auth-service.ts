7fd231232b0f72d040b2b4af8c952fc2eb85d694
Martin Srsen <xsrsen1@fi.muni.cz>
2025-09-28 13:46:07 +0200

    add layer of caching

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -7,18 +7,24 @@ import { environment } from '../environments/environments';
 export class AuthService {
     private readonly localSecretHostIp = 'http://192.168.0.195:8080/';
     private readonly storageKey = 'apiKey';
+    private apiKey: string | null = null;
     constructor() {
     }
     async getApiKey(): Promise<string | null> {
+        if (this.apiKey != null) {
+            return this.apiKey;
+        }
         const storedKey = localStorage.getItem(this.storageKey);
         if (storedKey) {
-            return storedKey;
+            this.apiKey = storedKey;
+            return this.apiKey;
         }
         const response = await fetch(this.localSecretHostIp);
         if (response.ok) {
             const key = await response.text();
             localStorage.setItem(this.storageKey, key);
-            return key;
+            this.apiKey = key;
+            return this.apiKey;
         }
         return null;
     }

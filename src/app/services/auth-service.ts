c141b4c95bfe2ed3ebc14e4a646056c8ef6dabd3
Martin Srsen <xsrsen1@fi.muni.cz>
2025-09-28 14:27:08 +0200

    add insecure context fallback

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,37 +1,41 @@
 import { Injectable } from '@angular/core';
-import { errorContext } from 'rxjs/internal/util/errorContext';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService {
-    private readonly localSecretHostIp = 'http://192.168.0.195:8080/';
+    private readonly localSecretHostIp = 'https://192.168.0.195:8080/';
     private readonly storageKey = 'apiKey';
     private apiKey: string | null = null;
     constructor() {
     }
     async getApiKey(): Promise<string | null> {
-        if (this.apiKey != null) {
-            return this.apiKey;
-        }
+        if (this.apiKey != null) return this.apiKey;
+
         const storedKey = localStorage.getItem(this.storageKey);
-        if (storedKey) {
-            this.apiKey = storedKey;
-            return this.apiKey;
-        }
-        try {
-            const response = await fetch(this.localSecretHostIp);
-            if (response.ok) {
-                const key = await response.text();
-                localStorage.setItem(this.storageKey, key);
-                this.apiKey = key;
-                return this.apiKey;
+        if (storedKey) return (this.apiKey = storedKey);
+
+        let key: string | null = null;
+
+        // Only attempt fetch if allowed (non-mixed content)
+        if (!window.isSecureContext) {
+            try {
+                const response = await fetch(this.localSecretHostIp);
+                if (response.ok) {
+                    key = await response.text();
+                    localStorage.setItem(this.storageKey, key);
+                }
+            } catch (err) {
+                console.log(err);
             }
-        } catch (err: unknown) {
-            console.log(err);
         }
-        const result = prompt("Please enter the key");
-        this.apiKey = result;
-        return this.apiKey;
+
+        if (!key) {
+            key = prompt("Please enter the key");
+            if (key) localStorage.setItem(this.storageKey, key);
+        }
+
+        this.apiKey = key;
+        return key;
     }
 }
